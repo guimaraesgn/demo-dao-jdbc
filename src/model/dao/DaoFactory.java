bf4bb432350010e0996c7fdbea31777296fbe0d6
unknown <guimaraesgn30@gmail.com>
2023-03-10 13:50:55 -0300

    final commit and db implements

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaojJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaojJDBC();
+		return new SellerDaojJDBC(DB.getConnection());
 	}
 
 }
